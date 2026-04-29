8242592ac0fbdb073a1e4243afa731375124e330
Argon <smmomm@126.com>
2026-04-29 19:39:20 +0800

    改错别字

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "RE:CodemaoAPI",
-  description: "下一个由社区维护的编程猫API文档",
+  description: "下一代由社区维护的编程猫API文档",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
